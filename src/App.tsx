b8e0f8df37a68ab980913b270da2571e5329204d
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-08-31 08:25:49 +0900

    feat: issue list를 위한 context 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import React, { useContext, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import GlobalStyle from 'GlobalStyle';
 import { Wrapper } from 'style/Wrapper';
+import { IssueContext } from 'contexts/IssueContext';
 
 function App() {
+  const { issues, fetchIssues, isLoading, isError } = useContext(IssueContext);
+
+  useEffect(() => {
+    fetchIssues();
+  }, []);
+
+  console.log(issues);
+  console.log('isLoading : ', isLoading);
+  console.log('isError : ', isError);
+
   return (
     <>
       <GlobalStyle />
