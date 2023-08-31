30b0629c61f78b87a91dede2a7fd8c87053f8a4e
plou102 <plou102@naver.com>
2023-08-31 22:06:07 +0900

    Feat: 리스트 api 연결 및 광고 구현

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,17 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { getIssues } from 'api/issueApi';
+import AdvertisementItem from 'components/Advertisement';
 import IssuesList from 'components/IssuesList';
-import { MOCK_DATA } from 'contexts/IssueContext';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import type { Issues } from 'types/Issue';
 
 const Home: React.FC = () => {
-  const data = MOCK_DATA;
-  return (
-    <>
-      <div>HomePage</div>
-      {data.map(list => {
-        return <IssuesList list={list} key={list.id} />;
-      })}
-    </>
-  );
+  const [listData, setListData] = useState<Issues>([]);
+
+  const getData = async () => {
+    const data = await getIssues('facebook', 'react', 1);
+    setListData(data);
+  };
+
+  useEffect(() => {
+    getData().catch(error => {
+      alert(error);
+    });
+  }, []);
+
+  const hasAdvertisement = (idx: number) => (idx + 1) % 4 === 0;
+
+  const issueElements = listData.map((list, idx) => (
+    <li key={list.id}>
+      <IssuesList list={list} key={list.id} />
+      {hasAdvertisement(idx) && <AdvertisementItem />}
+    </li>
+  ));
+
+  return <ul>{issueElements}</ul>;
 };
 
 export default Home;
