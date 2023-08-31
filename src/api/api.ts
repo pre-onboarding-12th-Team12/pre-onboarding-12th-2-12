940fa0bdfbc728f4506cc745f335a32f54291dc6
wlsgus93 <wlsgus63@naver.com>
2023-09-01 07:10:08 +0900

    Feat: 이슈 get api 추가

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,31 @@
-export const example = () => {
-  console.log('삭제해주세요');
+import axios, { AxiosInstance } from 'axios';
+
+const owner = 'facebook'; // 리포지토리 소유자의 GitHub 사용자명
+const repo = 'react'; 
+
+// axios 인스턴스 생성
+const instance: AxiosInstance = axios.create({
+  baseURL: `https://api.github.com/repos/${owner}/${repo}`, // 기본 URL 설정
+  headers: {
+    'Content-Type': 'application/json', // 기본 헤더 설정
+  }
+});
+
+// 토큰 값
+const authToken = process.env.REACT_APP_GITHUB_TOKEN;
+
+// 요청 전에 실행되는 인터셉터
+instance.interceptors.request.use(
+  config => {
+    config.headers.Authorization = `Bearer ${authToken}`;
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+
+export const getIssue = (issueNumber: number) => {
+  return instance.get(`/issues/${issueNumber}`);
 };
