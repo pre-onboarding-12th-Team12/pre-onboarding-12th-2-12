41756cc152fffcf4025ad24ea69c33e9e7d0cfab
kiseokDev <dev.leekiseok@gmail.com>
2023-09-01 15:34:23 +0900

    Feature: 무한스크롤 적용

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,35 +1,47 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
-import { getIssues } from 'api/issueApi';
+import useInfinityScroll from 'hooks/useInfinityScroll';
+import { useContext } from 'react';
+import { IssueContextProviderTemp } from 'contexts/issueContextTempForIssueList';
+import { styled } from 'styled-components';
 import AdvertisementItem from 'components/Advertisement';
 import IssuesList from 'components/IssuesList';
-import React, { useEffect, useState } from 'react';
-import type { Issues } from 'types/Issue';
 
 const Home: React.FC = () => {
-  const [listData, setListData] = useState<Issues>([]);
+  const { issues, fetchIssues, isLoading, isError } = useContext(
+    IssueContextProviderTemp
+  );
 
-  const getData = async () => {
-    const data = await getIssues('facebook', 'react', 1);
-    setListData(data);
-  };
+  const observeRef = useInfinityScroll({ callback: getIssuesCallback });
 
-  useEffect(() => {
-    getData().catch(error => {
-      alert(error);
-    });
-  }, []);
+  function getIssuesCallback() {
+    if (!isLoading) {
+      return fetchIssues().catch(error => {
+        console.error('Error fetching issues:', error);
+      });
+    }
+    return Promise.resolve(); // Always return a promise
+  }
 
-  const hasAdvertisement = (idx: number) => (idx + 1) % 4 === 0;
-
-  const issueElements = listData.map((list, idx) => (
+  const issueElements = issues.map((list, idx) => (
     <li key={list.id}>
       <IssuesList list={list} key={list.id} />
       {hasAdvertisement(idx) && <AdvertisementItem />}
     </li>
   ));
 
-  return <ul>{issueElements}</ul>;
+  return (
+    <ul>
+      {issueElements}
+      {isLoading && <>loading..</>}
+      {isError ? <>error가 발생하였습니다.</> : <ObserveRef ref={observeRef} />}
+    </ul>
+  );
 };
+const ObserveRef = styled.li`
+  height: 20px;
+`;
+
+const hasAdvertisement = (idx: number) => (idx + 1) % 5 === 0;
 
 export default Home;
--- src/page/IssueListPage.tsx
+++ src/page/IssueListPage.tsx
@@ -1,39 +1,14 @@
-import { getIssues } from 'api/issueApi';
 import useInfinityScroll from 'hooks/useInfinityScroll';
-import { useContext, useEffect, useRef, useState } from 'react';
-import { Issues } from 'types/Issue';
-import { ORGANIZATION, REPO } from 'utils/constants';
-import { useNavigate } from 'react-router-dom';
-import { IssueContext } from 'contexts/IssueContext';
-import IssueItem from 'components/issueList/IssueItem';
+import { useContext } from 'react';
 import { IssueContextProviderTemp } from 'contexts/issueContextTempForIssueList';
 import { styled } from 'styled-components';
-import AdvertisementItem from 'components/issueList/Advertisement';
-
-const hasAdvertisement = (idx: number) => (idx + 1) % 5 === 0;
-const ObserveRef = styled.li`
-  height: 20px;
-`;
+import AdvertisementItem from 'components/Advertisement';
+import IssuesList from 'components/IssuesList';
 
 export default function IssueListPage() {
-  const navigate = useNavigate();
   const { issues, fetchIssues, isLoading, isError } = useContext(
     IssueContextProviderTemp
   );
-
-  const [tempIssues, setTempIssues] = useState<Issues>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const fetchTempIssues = await getIssues(ORGANIZATION, REPO, 1);
-        setTempIssues(fetchTempIssues);
-      } catch (error) {
-        console.error('Error fetching temp issues:', error);
-      }
-    };
-    void fetchData();
-  }, []);
 
   const observeRef = useInfinityScroll({ callback: getIssuesCallback });
 
@@ -45,35 +20,26 @@ export default function IssueListPage() {
     }
     return Promise.resolve(); // Always return a promise
   }
-  const handleClickIssue = (issueNumber?: number) => {
-    if (issueNumber !== undefined) {
-      navigate(`/detail/${issueNumber}`);
-    }
-  };
 
-  // const issueElements = issues.map(
-  const issueElements = tempIssues.map(
-    ({ id, number, comments, title, user, updated_at }, issueIdx) => (
-      <li key={id}>
-        <IssueItem
-          issueNumber={number}
-          comments={comments}
-          title={title}
-          date={updated_at}
-          author={user.login}
-          handleClickIssue={handleClickIssue}
-        />
-        {hasAdvertisement(issueIdx) && <AdvertisementItem />}
-      </li>
-    )
-  );
+  const issueElements = issues.map((list, idx) => (
+    <li key={list.id}>
+      <IssuesList list={list} key={list.id} />
+      {hasAdvertisement(idx) && <AdvertisementItem />}
+    </li>
+  ));
 
   return (
     <ul>
       {issueElements}
       {isLoading && <>loading..</>}
       {isError ? <>error가 발생하였습니다.</> : <ObserveRef ref={observeRef} />}
-      {/* {isError ? <>error가 발생하였습니다.</> : <ObserveRef ref={observeRef} />} */}
     </ul>
   );
+}
+const ObserveRef = styled.li`
+  height: 20px;
+`;
+
+function hasAdvertisement(idx: number) {
+  return (idx + 1) % 5 === 0;
 }
