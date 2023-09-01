70a4d2c635e4762f1c6c26563bef77adb17184ad
wlsgus93 <wlsgus63@naver.com>
2023-09-01 17:41:03 +0900

    Fix conflit 해결

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from 'App';
 import Home from 'page/Home';
-<<<<<<< HEAD
-import Detail from '../page/Detail';
-=======
 import Detail from 'page/Detail';
->>>>>>> 751083396b8a62e9b2d2a3160d0a1fe538d040f8
 
 const router = createBrowserRouter([
   {
